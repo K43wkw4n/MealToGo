c72bf9217325047195037626e27191ae2971ca71
Kaewkwan <sanung2002@gmail.com>
2023-02-07 13:40:34 +0700

    Favourites Bar

--- src/features/restaurants/components/search.component.jsx
+++ src/features/restaurants/components/search.component.jsx
@@ -8,7 +8,7 @@ const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-export const Search = () => {
+export const Search = ({ isFavouritesToggled, onFavouritesToggle }) => {
     const { keyword, search } = useContext(LocationContext);
     const [searchKeyword, setSearchKeyword] = useState(keyword);
 
@@ -19,6 +19,8 @@ export const Search = () => {
     return (
         <SearchContainer>
             <Searchbar
+                icon={isFavouritesToggled ? "heart" : "heart-outline"}
+                onIconPress={onFavouritesToggle}
                 style={{ borderRadius: 100 / 2, }}
                 placeholder="Search for a location"
                 value={searchKeyword}
@@ -28,6 +30,7 @@ export const Search = () => {
                 onChangeText={(text) => {
                     setSearchKeyword(text);
                 }}
+                iconColor={isFavouritesToggled && 'red'}
             />
         </SearchContainer>
     );
--- src/services/favourites/favourites.context.jsx
+++ src/services/favourites/favourites.context.jsx
@@ -1,23 +1,18 @@
 import React, { createContext, useState } from "react";
 
-
 export const FavouritesContext = createContext();
-
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
-
 
   const add = (restaurant) => {
     setFavourites([...favourites, restaurant]);
   };
 
-
   const remove = (restaurant) => {
     const newFavourites = favourites.filter(
       (x) => x.placeId !== restaurant.placeId
     );
-
 
     setFavourites(newFavourites);
   };
